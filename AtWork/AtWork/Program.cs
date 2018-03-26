66c43555805292e897070e8181ea48021652da29
tylerjgabb <tylerjgabb@gmail.com>
2018-03-26 10:30:02 -0700

    added example to get test company name

--- AtWork/AtWork/Program.cs
+++ AtWork/AtWork/Program.cs
@@ -11,7 +11,7 @@ namespace AtWork
         static void Main(string[] args)
         {
             string format = "metadata=res://*/Model.csdl|res://*/Model.ssdl|res://*/Model.msl;provider=System.Data.SqlClient;provider connection string=\"{0}\"";
-            string connectionString = "data source=PP-PORSCHE-01;initial catalog=polyprint;persist security info=True;user id=sa;password=MngDBsvr2008@polyprint;MultipleActiveResultSets=True;App=EntityFramework";
+            string connectionString = "data source=PP-MUSTANG;initial catalog=polyprint;persist security info=True;user id=sa;password=MngDBsvr2008@polyprint;MultipleActiveResultSets=True;App=EntityFramework";
             using (var db = new polyprintEntities(String.Format(format, connectionString)))
             {
                 var requirements = db.QALabTests.Find(2).QALabTestMaps.Select(m => m.QALabRequirement);
@@ -19,6 +19,7 @@ namespace AtWork
                 {
                     Console.WriteLine(req.Name + " " + req.Description);
                 }
+                string company = db.QALabTests.Find(2).Address.Company;
 
             }
         }
