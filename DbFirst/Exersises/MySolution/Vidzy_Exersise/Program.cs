29a93b32df5add87a6548086d1f82f1951ea1eb1
tylerjgabb <tylerjgabb@gmail.com>
2018-04-20 10:15:38 -0700

    Updated Video model with Classification. Set to ENUM

--- DbFirst/Exersises/MySolution/Vidzy_Exersise/Program.cs
+++ DbFirst/Exersises/MySolution/Vidzy_Exersise/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Vidzy_Exersise
 {
@@ -10,7 +11,7 @@ namespace Vidzy_Exersise
     {
         static void Main(string[] args)
         {
-
+            
         }
     }
 }
--- DbFirst/Exersises/MySolution/Vidzy_Exersise/Video.cs
+++ DbFirst/Exersises/MySolution/Vidzy_Exersise/Video.cs
@@ -18,6 +18,7 @@ namespace Vidzy_Exersise
         public string Name { get; set; }
         public System.DateTime ReleaseDate { get; set; }
         public byte GenreId { get; set; }
+        public Classification Classification { get; set; }
     
         public virtual Genre Genre { get; set; }
     }
