8a567b95c26c157dfe5fb53994a62af476f99e70
tylerjgabb <tylerjgabb@gmail.com>
2018-04-21 10:36:32 -0700

    Added CodeFirstFromExistingDb. Enabled migrations and created first migration InitialModel

---
+++ CodeFirst/FromExisting/CodeFirstFromExistingDb/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace CodeFirstFromExistingDb.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<CodeFirstFromExistingDb.Model>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(CodeFirstFromExistingDb.Model context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
