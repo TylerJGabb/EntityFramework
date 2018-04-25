62cf12561bcbec2be8678ed3a88b5bd01ea94177
tylerjgabb <tylerjgabb@gmail.com>
2018-04-24 23:40:13 -0700

    added Queries sln

---
+++ Queries/Queries/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Data.Entity.Core.Common.CommandTrees;
+using System.Linq;
+
+namespace Queries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new PlutoContext())
+            {
+                //Get all courses in level 1, order by name descending then description descending
+                var courses =
+                    ctx.Courses
+                        .Where(c => c.Level == 1)
+                        .OrderByDescending(c => c.Name)
+                        .ThenByDescending(c => c.Description)
+                        .Select(c => new
+                        {
+                            Name = c.Name,
+                            Description = c.Description
+                        });
+
+            }
+        }
+    }
+}
